cf98eb5bb12b4567af81018edfe1492de70efcfb
Tobias Schneider <schneider@synyx.de>
2018-03-23 13:43:58 +0100

    Fix and improve tests of FrontpagePluginInterfaceImpl

--- src/test/java/coffee/synyx/frontpage/plugin/clock/FrontpagePluginInterfaceImplTest.java
+++ src/test/java/coffee/synyx/frontpage/plugin/clock/FrontpagePluginInterfaceImplTest.java
@@ -2,7 +2,6 @@ package coffee.synyx.frontpage.plugin.clock;
 
 import org.junit.Test;
 
-
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZonedDateTime;
@@ -11,19 +10,28 @@ import static org.hamcrest.CoreMatchers.is;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import static java.time.ZoneOffset.UTC;
+import static java.time.Clock.systemDefaultZone;
+import static java.time.ZoneId.systemDefault;
 
 
 public class FrontpagePluginInterfaceImplTest {
 
     @Test
+    public void hasCorrectTitle() {
+
+        final String title = new FrontpagePluginInterfaceImpl(systemDefaultZone()).title();
+        assertThat(title, is("Clock"));
+    }
+
+
+    @Test
     public void clockHasCorrectTime() {
 
-        final Instant fixedInstant = ZonedDateTime.of(2018, 11, 13, 0, 0, 0, 0, UTC).toInstant();
-        final Clock clock = Clock.fixed(fixedInstant, UTC);
+        final Instant fixedInstant = ZonedDateTime.of(2018, 11, 13, 0, 0, 0, 0, systemDefault()).toInstant();
+        final Clock clock = Clock.fixed(fixedInstant, systemDefault());
         final FrontpagePluginInterfaceImpl sut = new FrontpagePluginInterfaceImpl(clock);
 
-        final String render = sut.content();
-        assertThat(render, is("Hello CoffeeNet, it is 2018-11-13 01:00:00"));
+        final String content = sut.content();
+        assertThat(content, is("Hello CoffeeNet, it is 2018-11-13 12:00:00"));
     }
 }
